29eb241e58ef7ea58e7fdc2de028a137cb9bf3d0
apjanco <apjanco@gmail.com>
2023-10-03 13:29:30 -0400

    nope back to how it was

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,6 @@
-const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {  
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("admin");
-    eleventyConfig.addFilter('markdownify', (str) => {
-        return markdownItRenderer.render(str);
-      });
-    let options = {
-    html: true,
-    breaks: true,
-    linkify: true
-    };
 
-    eleventyConfig.setLibrary("md", markdownIt(options));
 };
