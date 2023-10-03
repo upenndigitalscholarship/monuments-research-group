1cc9e3a68a532e27739997e52f536642a7a48d43
apjanco <apjanco@gmail.com>
2023-10-03 13:21:20 -0400

    markdown it for the footnotes

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const markdownIt = require("markdown-it");
+const markdownItRenderer = new markdownIt();
+
 module.exports = function(eleventyConfig) {  
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addFilter('markdownify', (str) => {
+        return markdownItRenderer.render(str);
+      });
 };
