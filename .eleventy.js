ba8a276d8826d4df0c65172a23f548ab2b3a65b5
apjanco <apjanco@gmail.com>
2023-10-03 13:31:45 -0400

    actually try that again

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const markdownIt = require("markdown-it");
 
 module.exports = function(eleventyConfig) {  
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("admin");
+    let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+    };
 
+    eleventyConfig.setLibrary("md", markdownIt(options));
 };
